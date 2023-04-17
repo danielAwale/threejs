ef25467ad06472010f229e0b217b7050513ad01b
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 12:20:38 -0700

    finish work on the home page for now

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import {useSnapshot} from "valtio"
+
+//to get the color from the state that was defined in the store directory for valtio 
+
+import state from '../store'
+
 const CustomButton = ({type, title, customStyles, handleClick}) => {
+
+  const generateStyle = (type) => {
+    const snap = useSnapshot(state)
+    if(type === "filled") {
+      return {
+        backgroundColor: state.color,
+        color: '#fff'
+      }
+    }
+  }
   return (
-    <botton 
+    <button 
     className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
     style={generateStyle(type)}
-    >CustomButton</botton>
+    onClick={handleClick}
+    >CustomButton</button>
   )
 }
 
