f7ad24ea97515e07f2d7c7701ff49895fb204d48
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 11:34:06 -0700

    start work on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import {motion} from "framer-motion"
+//AnimatePresence enables the animation of components that have been removed from the tree.
+import {motion, AnimatePresence} from "framer-motion"
+
+// This hook actually returns a wrapped snapshot in a proxy for render optimization instead of a plain object compared to snapshot() method. Rule of thumb: read from snapshots, mutate the source. The component will only re-render when the parts of the state you access have changed, it is render-optimized.
+import {useSnapshot} from "valtio";
 
 const Home = () => {
   return (
