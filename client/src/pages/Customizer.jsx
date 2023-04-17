1e797b39f9493c3be3985836691c0eb261b4da84
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 13:36:01 -0700

    finnish with the initial imports

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { AnimationPresence, motion } from 'framer-motion';
-
 import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config';
+import { fadeAnimation, slideAnimation } from '../config/motion';
 
 const Customizer = () => {
   return <div>Customizer</div>;
