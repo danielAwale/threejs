45d3559ad965101ca5e4cea371ea467ddcd63101
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 11:56:55 -0700

    commence work on the home page jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,7 +17,29 @@ import {
 const Home = () => {
   const snap = useSnapshot(state);
   return (
-    <div>Home</div>
+   <AnimatePresence>
+    {snap.intro && (
+      <motion.section className='home' {...slideAnimation('left')}>
+        <motion.header {...slideAnimation("down")}>
+          <img src="./threejs.png" alt="logo" className='w-8 h-8 object-contain' />
+        </motion.header>
+
+        <motion.div className='home-content' {...headContainerAnimation}>
+          {/* the motion div below will contain the header */}
+          <motion.div {...headTextAnimation}>
+            <h1 className='head-text'>
+              LET'S <br className='xl:block hidden' /> DO IT!
+            </h1>
+          </motion.div>
+
+          <motion.div>
+            <p>Create your unique and exclusive shirt with our brand new 3d customiuzation tool. <strong>Unleash your imagination</strong> and define your own style</p>
+          </motion.div>
+          
+        </motion.div>
+      </motion.section>
+    )}
+   </AnimatePresence>
   )
 }
 
