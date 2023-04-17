b32f50f739811a9626ffb187b05bb4e2757ff4a9
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 12:10:05 -0700

    keep work going on the customize butotn '

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({type, title, customStyles, handleClick}) => {
   return (
-    <div>CustomButton</div>
+    <botton 
+    className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+    style={generateStyle(type)}
+    >CustomButton</botton>
   )
 }
 
