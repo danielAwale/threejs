9ea901bfc062722c2cc46b3d64d137195d3a25fe
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 13:31:58 -0700

    import all the necessary filesand packages to the customize page; also configure prettier

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { AnimationPresence, motion } from 'framer-motion';
+
+import { useSnapshot } from 'valtio';
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  return <div>Customizer</div>;
+};
 
-export default Customizer
+export default Customizer;
