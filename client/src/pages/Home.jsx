b53cede488c0fbce79c94787e685a72596d7cd1f
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 11:43:00 -0700

    import valtio config to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import {motion, AnimatePresence} from "framer-motion"
 // This hook actually returns a wrapped snapshot in a proxy for render optimization instead of a plain object compared to snapshot() method. Rule of thumb: read from snapshots, mutate the source. The component will only re-render when the parts of the state you access have changed, it is render-optimized.
 import {useSnapshot} from "valtio";
 
+import state from '../store'
+
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -13,6 +15,7 @@ import {
 } from '../config/motion'
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
     <div>Home</div>
   )
