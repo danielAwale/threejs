9a1260bdeca83eee2b27a93f0902fde7cb777452
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 20:47:17 -0700

    import the componenet to the index.js

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, Center } from '@react-three/drei';
 
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
 const CanvasModel = () => {
   return <div>Canvas</div>;
 };
