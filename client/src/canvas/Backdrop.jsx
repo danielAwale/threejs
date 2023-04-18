430aa5a4b7f46f9067b945f5bdc68eec767dd23c
danielAwale <danielmadhukarawale@gmail.com>
2023-04-17 20:45:03 -0700

    components for the canvas

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,8 @@
+// will be the back shadowy thing behind the shirt
+import React from 'react';
+
+const Backdrop = () => {
+  return <div>Backdrop</div>;
+};
+
+export default Backdrop;
---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,8 @@
+// will be for the movement of the camera
+import React from 'react';
+
+const CameraRig = () => {
+  return <div>CameraRig</div>;
+};
+
+export default CameraRig;
