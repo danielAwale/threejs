9d1cc31a698915998c9b9e86d278f0ea79241393
danielAwale <danielmadhukarawale@gmail.com>
2023-04-19 10:11:00 -0700

    contain some imports

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,5 +1,10 @@
 // will be for the movement of the camera
 import React from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshop } from 'valtio';
+
+import state from '../store';
 
 const CameraRig = () => {
   return <div>CameraRig</div>;
