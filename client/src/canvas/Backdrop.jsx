7a59a91b8177f7fc78475a00b3a62473bbcafc26
danielAwale <danielmadhukarawale@gmail.com>
2023-04-19 13:25:49 -0700

    contain imports in the backdrop component

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,5 +1,8 @@
 // will be the back shadowy thing behind the shirt
 import React from 'react';
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
   return <div>Backdrop</div>;
