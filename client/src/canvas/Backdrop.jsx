b98a4eeb4ab777971213732ec26bd603033fa651
danielAwale <danielmadhukarawale@gmail.com>
2023-04-19 13:41:15 -0700

    add more properties to the randomized light

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -16,7 +16,13 @@ const Backdrop = () => {
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
-      <RandomizedLight amount={4} />
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
       <RandomizedLight />
     </AccumulativeShadows>
   );
